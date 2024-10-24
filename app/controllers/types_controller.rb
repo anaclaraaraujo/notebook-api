1d5f0cf858660b434665cbc116c98b599f5af741
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-10-24 15:59:23 -0300

    feat: implement deserialization with AMS, relationships, and routes - Add deserialization of data using ActiveModelSerializers (AMS) - Implement relationships `belongs_to`, `has_many`, and `has_one` in models - Create routes for CRUD operations with nested attributes - Configure Nested Attributes to allow creation/update of related records - Integrate ActiveModelSerializers (AMS) for data serialization and deserialization

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -4,7 +4,6 @@ class TypesController < ApplicationController
   # GET /types
   def index
     @types = Type.all
-
     render json: @types
   end
 
@@ -39,13 +38,19 @@ class TypesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_type
-      @type = Type.find(params[:id])
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_type
+    if params[:contact_id]
+      @type = Contact.find(params[:contact_id]).type
+      return @type
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def type_params
-      params.require(:type).permit(:description)
-    end
+      @type = Type.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def type_params
+    params.require(:type).permit(:description)
+  end
 end
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,11 +1,17 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthday
   belongs_to :type do 
-    link(:related) { type_url(object.id) }
+    link(:related) { contact_type_url(object.id) }
   end
 
-  has_many :phones
-  has_one :address
+  has_many :phones do 
+    link(:related) { contact_phones_url(object.id) }
+  end
+
+  has_one :address do 
+    link(:related) { contact_address_url(object.id) }
+  end
+
 
   def attributes(*args)
     h = super(*args)
